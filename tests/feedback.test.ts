471971570db6eeeceaf41a8caa0f0ad29a861703
Yaroslav Panchishyn <yarpan@gmail.com>
2024-02-01 22:34:32 +0200

    refactoring

--- tests/feedback.test.ts
+++ tests/feedback.test.ts
@@ -1,12 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { HomePage } from "../pages/homePage";
 import { ContactsPage } from "../pages/contactsPage";
-
-test('Open home page', async ({ page }) => {
-    const homePage = new HomePage(page);
-    await homePage.goto();
-    await homePage.isPageLoaded();
-})
 
 test('Show validation on sending empty feedback', async ({ page }) => {
     const contactPage = new ContactsPage(page);
