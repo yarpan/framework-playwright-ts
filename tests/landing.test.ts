6360ca6616691f89c5b41d778419bdfcc20f8bb3
Yaroslav Panchishyn <yarpan@gmail.com>
2024-03-21 22:17:29 +0200

    refactoring

--- tests/landing.test.ts
+++ tests/landing.test.ts
@@ -1,17 +1,17 @@
 import { test, expect } from "@playwright/test";
 import { LandingPage } from "../pages/landing";
 
-test.only("Check if LandingPage loading", async ({ page }) => {
+test("Check if LandingPage loading", async ({ page }) => {
   const landingPage = new LandingPage(page);
   await landingPage.goto();
   await landingPage.isPageLoaded();
-  //await expect(page).toHaveScreenshot();
-  await expect(page).toHaveScreenshot({ fullPage: true });
+  await expect(page).toHaveScreenshot();
+  //await expect(page).toHaveScreenshot({ fullPage: true });
   //xpect(await page.screenshot({ fullPage: true })).toMatchSnapshot('./snapshots/landing-page-chromium-win32.png');
 
 });
 
-test.skip("Check Links for different locales ", async ({ page }) => {
+test("Check Links for different locales ", async ({ page }) => {
   const landingPage = new LandingPage(page);
   for (let i = 0; i < landingPage.localeData.length; i++) {
     await test.step(`Check for ${landingPage.localeData[i][0]} language`, async () => {
---
+++ tests/manage-multiwin.test.ts
@@ -0,0 +1,26 @@
+import { test, expect, chromium } from "@playwright/test";
+import { LandingPage } from "../pages/landing";
+
+test("Has title", async () => {
+    let browser = await chromium.launch();
+    let page = await browser.newPage();
+    await page.goto('https://www.wikipedia.org')
+
+    expect (await page.title()).toContain("Wikipedia");
+    await browser.close();
+});
+
+
+test("Two Pages", async () => {
+    let browser = await chromium.launch();
+    const context = await browser.newContext();
+    const pageOne = await context.newPage();
+    const pageTwo = await context.newPage();
+
+    await pageOne.goto("https://www.wikipedia.org");
+    await pageTwo.goto("https://playwright.dev");
+
+    await browser.close();
+});
+
+
