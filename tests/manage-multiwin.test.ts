aa497df73a434ccfa805efb15b12d401b1ab5fd9
Yaroslav Panchishyn <yarpan@gmail.com>
2024-03-25 22:34:05 +0200

    page management

--- tests/manage-multiwin.test.ts
+++ tests/manage-multiwin.test.ts
@@ -24,3 +24,34 @@ test("Manage two Pages", async () => {
 });
 
 
+test("check modal closed", async ({page})=> {
+    await page.goto("https://www.phn.sterbc.com/login");
+    await page.locator("//a[@id='signUp']").click();
+
+    console.log(await page.locator("//div[@id='online-access']//p[1]").innerText);
+    await page.locator("//div[@id='online-access']//button[@class='icon-float']").click();
+})
+
+
+test("check modal actions", async ({page, context, browser})=> {
+    await page.goto("https://www.phn.sterbc.com/login");
+    await page.locator("//a[@id='signUp']").click();
+
+    let someText = await page
+        .locator("//div[@id='online-access']//p[1]")
+        .innerText();
+    
+    await page.locator("//div[@id='online-access']//a").click();
+    
+    // wait untill context load all tabs
+    await page.waitForTimeout(1000);
+
+    let pages = await context.pages();
+    console.log(pages.length);
+
+    await pages[1].close();
+    await page.locator("asd").click();
+
+    let contexts = await browser.contexts();
+    let pageOneFirstContext: Page = await contexts[0].pages[0]; 
+})
