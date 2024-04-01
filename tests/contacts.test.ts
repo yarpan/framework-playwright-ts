88e2836202d4bb8b22d15e495170f72276376033
Yaroslav Panchishyn <yarpan@gmail.com>
2024-04-01 18:23:00 +0300

    fixtures

--- tests/contacts.test.ts
+++ tests/contacts.test.ts
@@ -1,14 +1,10 @@
-import { test, expect } from "@playwright/test";
-import { ContactsPage } from "../pages/contacts";
+import { test, expect } from './fixtures';
 import { MainEnPage } from "../pages/main";
 import testSuiteBodyLinks from "../pages/contacts.BodyLinks.json";
 import testSuiteLeftMenu from "../pages/contacts.LeftMenu.json";
 
-test("Check BackToMainPage Button", async ({ page }) => {
-  const contactUsPage = new ContactsPage(page);
-  await contactUsPage.goto();
-  await contactUsPage.isPageLoaded();
-  await contactUsPage.clickButtonBack();
+test("Check BackToMainPage Button", async ({ page, contactsPage }) => {
+  await contactsPage.clickButtonBack();
 
   const wikiEnMainPage = new MainEnPage(page);
   await wikiEnMainPage.isPageLoaded();
@@ -16,26 +12,19 @@ test("Check BackToMainPage Button", async ({ page }) => {
 
 test.describe("Check Body Links - DataDriven by JSON", () => {
   for (const testCase of testSuiteBodyLinks) {
-    test(testCase.testName, async ({ page }) => {
+    test(testCase.testName, async ({ page, contactsPage }) => {
       console.log("Test Case: " + testCase.testName);
-      const contactUsPage = new ContactsPage(page);
-      await contactUsPage.goto();
-      await contactUsPage.isPageLoaded();
-      await contactUsPage.validateLink(testCase.linkLocator, testCase.linkUri);
+      await contactsPage.validateLink(testCase.linkLocator, testCase.linkUri);
     });
   }
 });
 
 test.describe("Check Left Menu Items - DataDriven by JSON", () => {
   for (const testCase of testSuiteLeftMenu) {
-    test(testCase.testName, async ({ page }) => {
+    test(testCase.testName, async ({ page, contactsPage }) => {
       console.log("Test Case: " + testCase.testName);
-      const contactUsPage = new ContactsPage(page);
-      await contactUsPage.goto();
-      await contactUsPage.isPageLoaded();
-
-      await contactUsPage.openSubPage(testCase.subPageLocator);
-      await contactUsPage.isSubPageLoaded(
+      await contactsPage.openSubPage(testCase.subPageLocator);
+      await contactsPage.isSubPageLoaded(
         testCase.subPageUrl,
         testCase.subPageTitle,
         testCase.subPageUniqueText
